3dc0cb5c383474d3af8b413af2bec2d1b3dc472a
ali-yigit <alygtalygt@gmail.com>
2023-03-06 10:25:41 +0300

    mytwitter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from "react";
+import { ThemeProvider } from "styled-components";
 
-function App() {
+// Theme
+import { themeSettings } from "./theme";
+
+// Styled Components
+import Button from "./components/Button";
+import CloseIcon from "./components/CloseIcon";
+import CommentIcon from "./components/CommentIcon";
+import GlobalStyle from "./components/GlobalStyle";
+import {
+  ModalButtonWrapper,
+  ModalContent,
+  ModalParagraph,
+  ModalTitle,
+  ModalWrapper
+} from "./components/Modal";
+
+const App = () => {
+  const [theme, toggleTheme] = useState("light");
+
+  const newTheme = theme === "light" ? "dark" : "light";
+
+  const setTheme = () => {
+    toggleTheme(newTheme);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <GlobalStyle />
+      <ThemeProvider theme={themeSettings[theme]}>
+        <ModalWrapper>
+          <CloseIcon />
+          <ModalContent>
+            <CommentIcon />
+            <ModalTitle>Reply to join the conversation.</ModalTitle>
+            <ModalParagraph>
+              Once you join Twitter, you can respond to Ali YİĞİT's Tweet
+            </ModalParagraph>
+            <ModalButtonWrapper>
+              <Button type="primary">Log in</Button>
+              <Button type="secondary" onClick={setTheme}>
+                Set {newTheme} Theme
+              </Button>
+            </ModalButtonWrapper>
+          </ModalContent>
+        </ModalWrapper>
+      </ThemeProvider>
+    </Fragment>
   );
-}
+};
 
 export default App;
